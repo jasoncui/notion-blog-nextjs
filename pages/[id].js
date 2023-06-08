2914749919ce08a91d453b1a5a84b9e33eafa99b
Jason Cui <jason.s.cui11@gmail.com>
2023-06-08 10:30:46 -0400

    open link in new tab

--- pages/[id].js
+++ pages/[id].js
@@ -26,7 +26,13 @@ export const Text = ({ text }) => {
         ].join(" ")}
         style={color !== "default" ? { color } : {}}
       >
-        {text.link ? <a href={text.link.url}>{text.content}</a> : text.content}
+        {text.link ? (
+          <a target="_blank" rel="noopener noreferrer" href={text.link.url}>
+            {text.content}
+          </a>
+        ) : (
+          text.content
+        )}
       </span>
     );
   });
