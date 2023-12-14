af6c0bb88279e94f1f0b72f8ae3c87fc6bd47398
Jason Cui <jason.s.cui11@gmail.com>
2023-12-14 14:03:49 -0800

    update to geist font

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,15 @@
 import "../styles/globals.css";
 import { Analytics } from "@vercel/analytics/react";
+import { GeistSans } from 'geist/font/sans';
+import { GeistMono } from 'geist/font/mono';
+
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <main className={GeistSans.className}>
       <Component {...pageProps} />
       <Analytics />
-    </>
+    </main>
   );
 }
 
