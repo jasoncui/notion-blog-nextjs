624e64093275c65a591afe25afaf7dc464ad79fb
Jason Cui <jason.s.cui11@gmail.com>
2024-05-02 11:21:59 -0700

    remove font

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,9 @@
 import "../styles/globals.css";
 import { Analytics } from "@vercel/analytics/react";
-import { GeistSans } from 'geist/font/sans';
-import { GeistMono } from 'geist/font/mono';
-
 
 function MyApp({ Component, pageProps }) {
   return (
-    <main className={`${GeistSans.className}`} >
+    <main>
       <Component {...pageProps} />
       <Analytics />
     </main>
